71d11bb269e500c14c01b67adc1d1c053f28682d
Aleksandra Nikolaenko <littlejellycat@yandex.ru>
2016-08-23 12:40:57 +0300

    Day 03 part 2 + minor change in part 1

--- src/Spherical.kt
+++ src/Spherical.kt
@@ -1,11 +1,10 @@
-import org.jetbrains.annotations.Mutable
 import java.io.File
 
 /**
 * Created by Jelly on 23.08.2016.
 */
 
-fun sumHouses(trace: String): Int{
+fun sumHouses(trace: String): MutableSet<Pair<Int, Int>>{
     var x = 0
     var y = 0
     val setOfCoors: MutableSet<Pair<Int, Int>> = mutableSetOf(x to y)
@@ -18,10 +17,16 @@ fun sumHouses(trace: String): Int{
         }
         setOfCoors.add(x to y)
     }
-    return setOfCoors.size
+    return setOfCoors //now returns the set itself
+}
+
+fun sumHousesForBoth(trace: String): Set<Pair<Int, Int>>{
+    return (sumHouses(trace.filterIndexed { i, c -> i % 2 == 0 })
+            .union(sumHouses(trace.filterIndexed { i, c -> i % 2 != 0 })))
 }
 
 fun main(args: Array<String>) {
     val trace = File("C:\\Users\\kotat\\input.txt").readText()
-    println(sumHouses(trace))
+    println(sumHouses(trace).size)
+    println(sumHousesForBoth(trace).size)
 }
