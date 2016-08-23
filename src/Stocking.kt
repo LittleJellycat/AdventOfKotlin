0d877688fc1cf6765a348af22b18d9ea2c83152d
Aleksandra Nikolaenko <littlejellycat@yandex.ru>
2016-08-23 14:02:47 +0300

    Re-uploaded day 04

--- src/Stocking.kt
+++ src/Stocking.kt
@@ -1,3 +1,19 @@
+import java.io.File
+import java.security.MessageDigest
+import javax.xml.bind.DatatypeConverter
+
 /**
- * Created by anikolae on 23.08.2016.
+ * Created by Jelly on 23.08.2016.
  */
+fun main(args: Array<String>) {
+    val key = File("C:\\Users\\kotat\\input.txt").readText()
+    println(findHashWithZeroes(key, 5)) //part 1
+    println(findHashWithZeroes(key, 6)) //part 2
+}
+
+fun findHashWithZeroes(key: String, numberOfZeroes: Int): Int {
+    return (1..Int.MAX_VALUE).first {DatatypeConverter.printHexBinary(
+        MessageDigest.getInstance("MD5")
+                .digest((key + it).toByteArray())).startsWith("0".repeat(numberOfZeroes))
+    }
+}
