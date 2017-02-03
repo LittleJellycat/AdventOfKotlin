953d54e9a2487b0ff3355483025729e0ee26468d
littlejellycat <littlejellycat@yandex.ru>
2017-02-03 20:53:35 +0300

    Day 11

---
+++ src/Infinite.kt
@@ -0,0 +1,22 @@
+import java.util.*
+
+/*This solution takes too long to calculate the answer. The output is correct though.
+* I believe, there should be a much faster solution*/
+
+fun main(args: Array<String>) {
+    val endNumber = 29000000 //given input
+    val numberOfPresentsList = ArrayList<Int>()
+    var n = 1
+    numberOfPresentsList.add(0, 0)
+    while (true) {
+        numberOfPresentsList.add(n, 0)
+        (1..n)
+                .filter { n % it == 0 }
+                .forEach { numberOfPresentsList[n] += it * 10 }
+        if (!numberOfPresentsList.filter { n >= endNumber }.isEmpty()) break
+        n++
+        if (numberOfPresentsList.max()!! >= endNumber) break
+        if (n > endNumber / 10) break
+    }
+    println(numberOfPresentsList.indexOfFirst { it >= endNumber })
+}
---
+++ src/LookSay.kt
@@ -0,0 +1,29 @@
+fun main(args: Array<String>) {
+    val input = "1113122113"
+    var saidInput = input.toList()
+    for (i in 0..51) {
+        saidInput = applyGameRules(saidInput)
+        println("$i ${saidInput.count()}")
+    }
+}
+
+fun applyGameRules(input: List<Char>): List<Char> {
+    var stackOfToken = 0
+    var lastToken = input.first()
+    val outSequence = mutableListOf<Char>()
+    input.forEach {
+        if (it == lastToken) {
+            stackOfToken++
+        } else {
+            outSequence.add(stackOfToken.toString().first())
+            outSequence.add(lastToken)
+            lastToken = it
+            stackOfToken = 1
+        }
+    }
+    lastToken = input.last()
+    outSequence.add(stackOfToken.toString().first())
+    outSequence.add(lastToken)
+
+    return outSequence
+}
